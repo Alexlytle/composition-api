65a5b2c2f662e9e32e5266f0c0e513df1acd552c
Alexlytle <44213950+Alexlytle@users.noreply.github.com>
2021-05-24 17:35:04 -0700

    Vue Composition state manegment
    Vue Composition state manegment

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>This is an about page{{store.state.counter}}</h1>
+  </div>
+</template>
+<script>
+import {inject} from 'vue'
+export default {
+   setup() {
+    const store = inject('store')
+  
+     return {store}
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div 
+    :style="{color:store.state.colorCode}"
+    class="counter">
+      {{store.state.counter}}
+    </div>
+    <div class="countersquared">
+      {{store.state.counter}}
+      <sup>2</sup> = 
+      {{store.getters.countersquared()}}
+    </div>
+    <div class="buttons">
+        <button  @click = 'store.methods.decreaseCounter'>-</button>
+        <button @click = 'store.methods.increaseCounter'>+</button>
+    </div>
+
+    <input v-model = 'colorCode' type="text" placeholder="enter color code">
+  </div>
+ 
+</template>
+
+<script>
+// @ is an alias to /src
+
+import {inject,computed} from 'vue'
+export default {
+  name: 'Home',
+  setup() {
+    const store = inject('store')
+
+    const colorCode = computed({
+        get(){
+          return store.state.colorCode
+        },
+        set(val){
+          store.methods.setColorCode(val)
+        }
+
+    })
+
+
+  
+
+  
+     return {store,colorCode}
+  }
+ 
+}
+</script>
+<style>
+.counter{
+  font-size: 80px;
+}
+.buttons button{
+  font-size: 60px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
